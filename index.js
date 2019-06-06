0ea3451c5bc9123d2baec9e4335df2c1ac4ccce3
Jari Pohjasmäki <japohjas@gmail.com>
2019-06-07 01:25:40 +0300

    eka

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-532523",
+    id: 2
+  },
+  {
+    name: "Dan Abramow",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+]
+
+app.use(cors())
+app.use(bodyParser.json())
+
+// npm install --save morgan
+// https://stackoverflow.com/questions/51409771/logging-post-body-size-using-morgan-when-request-is-received
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+  const info = `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  res.send(info)
+})
+
+app.get('/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  } 
+})
+
+app.delete('/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/persons/', (request, response) => {
+  const body = request.body
+  
+  if (persons.find(p => p.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+ 
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000000) + 1
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
